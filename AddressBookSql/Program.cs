c5152eae511eb1182fa703c430d09d73df684509
naren0706 <narenthrakishhore@gmail.com>
2023-08-31 15:00:12 +0530

    [Narenthra Kishore] Add : new function to update values using name

--- AddressBookSql/Program.cs
+++ AddressBookSql/Program.cs
@@ -8,8 +8,20 @@ namespace AddressBookSql
         {
             Operation operation = new Operation();
             //operation.CreateTable();
-            List<AddressBook> list = operation.CreateRecords();
-            operation.CreateRecords();
+            //List<AddressBook> list = operation.CreateRecords();
+            //operation.CreateRecords();
+            AddressBook book = new AddressBook()
+            {
+                FirstName = "name2",
+                LastName = "name2",
+                Address = "street2",
+                City = "UpdatedCity",
+                State = "UpdatedState",
+                Zip = "666666",
+                PhoneNumber = "999999999",
+                Email = "azasf@gmail.com"
+            };
+            operation.UpdateDetails(book);
         }
     }
 }
