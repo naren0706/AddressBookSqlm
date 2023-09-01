c5cb882b3afc31eeb0a8c077da2e5d77adcd6644
naren0706 <narenthrakishhore@gmail.com>
2023-09-01 16:32:39 +0530

    [Narenthra Kishore]

--- AddressBookSql/Program.cs
+++ AddressBookSql/Program.cs
@@ -24,13 +24,7 @@ namespace AddressBookSql
             //operation.UpdateDetails(book);
             //operation.GroupByCity();
             //operation.GroupByState();
-<<<<<<< HEAD
-            //operation.SameCityDetails("Chennai");
-            operation.DeleteContact("name2");
-=======
-            operation.GetCitySize();
-            operation.GetStateSize();
->>>>>>> UC7-StateNCitySize
+
         }
     }
 }
